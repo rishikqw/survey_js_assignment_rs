cd1c7dbbb9c7815f549005b0f10df3ba615567d2
rishikqw <rishik@quickwork.co>
2025-01-02 17:11:07 +0530

    updated env variable names in mySql.js

--- src/config/mySql.js
+++ src/config/mySql.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectionParams = {
-  host: process.env.HOST,
-  port: process.env.PORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 const db = mysql.createConnection(connectionParams);
